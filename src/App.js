6a00ae81411f2bff9b22941823f4ae340cc883f2
voinot <flovno@gmail.com>
2020-06-24 22:37:32 +0200

    init prescription page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import PrescriptionPage from './PrescriptionPage'
 
 function App() {
   return (
     <div className="App">
-      Hello Word !
+ <PrescriptionPage />
     </div>
   );
 }
---
+++ src/PrescriptionPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const PrescriptionPage = () => {
+
+    return (
+
+        <form className="">
+            <div>
+                <label for=""></label>
+                <input type="" name="" placeholder="">
+          </div>
+                <div>
+                    <label for=""></label>
+                    <input type="text" name="">
+</div>
+
+                    <button type="submit"></button>
+    </form>
+   )   
+}
+
+export default PrescriptionPage
