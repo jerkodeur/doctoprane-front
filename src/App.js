e07f14abe6889644a3b8125924cdaa65c87a2b51
Greta Mezzina <gretamezzina@Air-di-Greta>
2020-06-24 22:34:12 +0200

    routes created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage.js';
+import MonitoringPage from './components/MonitoringPage';
+import PrescriptionPage from './components/PrescriptionPage';
+import PatientPage from './components/PatientPage'
 
 function App() {
   return (
-    <div className="App">
-      Hello Word !
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/doctor_monitoring" component={MonitoringPage} />
+        <Route path="/doctor_prescription" component={PrescriptionPage} />
+        <Route path="/patient" component={PatientPage} />
+      </Switch> 
+    </Router>
+  )
 }
 
 export default App;
