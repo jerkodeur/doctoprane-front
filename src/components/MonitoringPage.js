bff424875395c815350d99f184292edd08d2fe84
jerkodeur <jerome.potie@gmail.com>
2020-06-25 15:55:07 +0200

    fetch user datas

--- src/components/MonitoringPage.js
+++ src/components/MonitoringPage.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 import './MonitoringPage.css'
 
 const MonitoringPage = (props) => {
-  console.log(props.location.data.patient_id)
+  // const doctor_id = props.location.data.doctor_id
+  const doctor_id = 1
+
+  useEffect(() => {
+    axios.get(`http://localhost:3300/doctors/${doctor_id}/patient/1`)
+      .then(res => console.table(res.data[0]))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <>
       Hello World
